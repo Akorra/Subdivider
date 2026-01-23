f4059cf991d48810ab5a62baf70eda77a693a5a3
Akorra <neverfearphilishere@gmail.com>
2026-01-23 01:04:18 +0000

    added glad and gflw to consummer app

--- consumerApp/main.cpp
+++ consumerApp/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+
 #include "subdiv.hpp"
+#include "app.hpp"
 
 int main() {
-    Subdiv::Init();
+    App app(800, 600, "Subdivier Test");
+
+    if (!app.Init()) return -1;
+
+    app.Run();
     return 0;
 }
