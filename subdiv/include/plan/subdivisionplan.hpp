b6b02fcb3efcd284ba83bd3deaf2f2606ce70f7c
Akorra <neverfearphilishere@gmail.com>
2026-05-01 00:51:19 +0100

    added node types enum

--- subdiv/include/plan/subdivisionplan.hpp
+++ subdiv/include/plan/subdivisionplan.hpp
@@ -5,6 +5,15 @@
 namespace Subdiv::Plan
 {
 
+enum class NodeType : uint8_t {
+    Internal     = 0,  // has 4 children, recurse
+    Regular      = 1,  // bicubic B-spline, 16 control points
+    Boundary     = 2,  // like Regular, extrapolated phantom pts
+    Crease       = 3,  // Regular + one semi-sharp edge
+    Terminal     = 4,  // collapses n levels of EV subtree
+    Extraordinary= 5,  // EV corner: limit pos + 2 tangents only
+};
+
 /**
  * @brief SubdivisionPlan
  *  
